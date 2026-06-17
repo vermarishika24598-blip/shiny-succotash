bf43d4523552bbb6e2878241b722f9be800b9ef1
Rishika Verma <vermarishika24598@gmail.com>
2026-06-17 12:43:22 +0530

    updated

--- pages/App.jsx
+++ pages/App.jsx
@@ -1,13 +1,24 @@
+import { useEffect } from "react";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
 import { About } from "./About";
 import { Project } from "./project";
 import { Contact } from "./Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
+// Ye chota sa component scroll ko top par reset karega
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Isse page hamesha top par load hoga */}
       <Header />
       <main>
         <Routes>
